f4f3a56df7bbbceb49a4f49da4a6943f9cde0cfb
Juan Benitez <juanbenitez2908@gmail.com>
2019-04-11 00:02:22 -0500

    creating first basic server with graphql, adding queries and mutations

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/', (req, res, err)=>{
-    res.send('Hello World!');
-});
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+
+    `),
+    rootValue: {
+        events: () => {
+            return ['Cooking', 'Sailing', 'Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
