4d6e46d291494b5a9f45b2c8a299b1e0f7a9933c
Juan Benitez <juanbenitez2908@gmail.com>
2019-04-13 19:10:55 -0500

    finish pushing

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const Event = require('./models/event');
+// Models
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: buildSchema(`
         type Event {
             _id: ID!
@@ -18,6 +23,12 @@ app.use('/graphql', graphqlHttp({
             description: String!
             price: Float!
             date: String!
+        }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
         }
         
         input EventInput {
@@ -27,12 +38,18 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
         }
         
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User    
         }
 
         schema {
@@ -42,43 +59,84 @@ app.use('/graphql', graphqlHttp({
 
     `),
     rootValue: {
-        events: () => {
-            return Event.find().then(events =>{  // Return is for express-graphql knows that is an async and we need to wait 
-                return events.map(event =>{
-                    return { ...event._doc };
-                });   
-            }).catch(err =>{
-                throw err;
-            })
-        },
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+      events: () => {
+        return Event.find()
+          .then(events => {
+            // Return is for express-graphql knows that is an async and we need to wait
+            return events.map(event => {
+              return { ...event._doc };
             });
-
-            return event
-                .save()
-                .then(result =>{
-                    console.log(result);
-                    return { ...result._doc }; // Leave out all metadata
-                }).catch(err => {
-                    console.log(err);
-                    throw err; // Return an error
-                });
-        }
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: "5cb27257ee98b931b48a3fe6"
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then(result => {
+            createdEvent = { ...result._doc };
+            return User.findById("5cb27257ee98b931b48a3fe6");
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error("User doesnt exist");
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+              return createdEvent;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err; // Return an error
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User exists already.");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null };
+          })
+          .catch(err => {
+            throw err;
+          });
+      }
     },
     graphiql: true
   })
 );
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-gxoow.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
-.then(() => {
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-gxoow.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  )
+  .then(() => {
     app.listen(3000);
-})
-.catch(err => {
+  })
+  .catch(err => {
     console.log(err);
-});
-
+  });
